1016ca04b376858df26c5637a0d98cec5070dda4
JaceGarcia <jaces.place@gmail.com>
2017-07-19 18:12:32 -0400

    orders

--- homework/unit_02/pizza-express/app.js
+++ homework/unit_02/pizza-express/app.js
@@ -5,6 +5,19 @@ const hbs = require('hbs');
 app.set("view engine", "hbs");
 
 
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+    res.send('Welcome to Pizza Express!');
+});
+
+const toppingsController = require('./controllers/toppings_controller');
+    app.use('/Toppings', toppingsController);
+
+const orderController = require('./controllers/order_controllers');
+    app.use('/order', orderController);
+
+
 const port    = 3001;
 
 app.listen(port, function(){
---
+++ homework/unit_02/pizza-express/controllers/toppings_controller.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const router = express.Router();
+
+router.get('/:type', (req, res) => {
+    res.send(`${req.params.type} pizza! Good choice.`);
+});
+
+module.exports = router;
