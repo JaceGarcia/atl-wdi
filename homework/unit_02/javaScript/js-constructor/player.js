b360f298af56b20a719db63d50fb89f952ad4f4e
JaceGarcia <jaces.place@gmail.com>
2017-07-24 17:52:17 -0400

    part 1: created Tamagotchi constructor

--- homework/unit_02/javaScript/js-constructor/player.js
+++ homework/unit_02/javaScript/js-constructor/player.js
@@ -1,6 +1,6 @@
 console.log('player file is loaded');
 
-var player = {
+var player1 = {
   name: "Neo",
   feedTamagotchi: function(tamagotchi) {
     tamagotchi.foodInTummy++;
@@ -14,4 +14,36 @@ var player = {
     tamagotchi.restedness++;
     console.log(tamagotchi.name + ' got some Zzzzs! Restedness: ' + tamagotchi.restedness);
   }
-}; //end player
+};
+
+var player2 = {
+  name: "Morpheus",
+  feedTamagotchi: function(tamagotchi) {
+    tamagotchi.foodInTummy++;
+    console.log('YUM!!! ' + tamagotchi.name + ' has been fed and now has: ' + tamagotchi.foodInTummy + ' in the tummy');
+  },
+  medicateTamagotchi: function(tamagotchi) {
+    tamagotchi.health++;
+    console.log(tamagotchi.name + ' has been medicated and now has health: ' + tamagotchi.health);
+  },
+  knockOutTamagotchi: function(tamagotchi) {
+    tamagotchi.restedness++;
+    console.log(tamagotchi.name + ' got some Zzzzs! Restedness: ' + tamagotchi.restedness);
+  }
+};
+
+var player1 = {
+  name: "Trinity",
+  feedTamagotchi: function(tamagotchi) {
+    tamagotchi.foodInTummy++;
+    console.log('YUM!!! ' + tamagotchi.name + ' has been fed and now has: ' + tamagotchi.foodInTummy + ' in the tummy');
+  },
+  medicateTamagotchi: function(tamagotchi) {
+    tamagotchi.health++;
+    console.log(tamagotchi.name + ' has been medicated and now has health: ' + tamagotchi.health);
+  },
+  knockOutTamagotchi: function(tamagotchi) {
+    tamagotchi.restedness++;
+    console.log(tamagotchi.name + ' got some Zzzzs! Restedness: ' + tamagotchi.restedness);
+  }
+};
--- homework/unit_02/javaScript/js-constructor/tamagotchi.js
+++ homework/unit_02/javaScript/js-constructor/tamagotchi.js
@@ -1,6 +1,18 @@
 console.log('tamagotchi file is loaded');
 
 //your constructor function here
+var tomagotchi = function(name, creatureType){
+    this.name = name;
+    this.creatutreValue = creatureType;
+    this.foodInTummy = 100;
+    this.readyness = 100;
+    this.health = 100;
+    this.cry = function(){
+        this.foodInTummy --;
+        console.log("WAHH!!!");
+        console.log(this.name 'I have food in my tummy' + this.foodInTummy);
+    };
+}
 
 
 //create new Tamagotchis
