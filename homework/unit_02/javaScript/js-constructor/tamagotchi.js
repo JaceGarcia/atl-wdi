0884c6bb108056f9756e85b209e4303bc2893246
JaceGarcia <jaces.place@gmail.com>
2017-07-24 18:23:12 -0400

    part 4 added new methods

--- homework/unit_02/javaScript/js-constructor/tamagotchi.js
+++ homework/unit_02/javaScript/js-constructor/tamagotchi.js
@@ -25,6 +25,25 @@ class Tamagotchi{
       console.log('Yaaaawwwwn! So tired');
       console.log(this.name + ' has current restedness = ' + this.restedness);
   }
+  this.start = function(){
+    console.log("Starting " + this.name);
+    var self = this;
+    this.hungerTimer = setInterval(function() {
+      self.cry();
+    }, 6000);
+    this.yawnTimer = setInterval(function() {
+      self.yawn();
+    }, 10000);
+    this.sickTimer = setInterval(function() {
+      self.puke();
+    }, 25000);
+  };
+  this.stop = function(){
+    console.log("Stopping " + this.name);
+    clearInterval(this.hungerTimer);
+    clearInterval(this.yawnTimer);
+    clearInterval(this.sickTimer);
+  };
 }
 
 
