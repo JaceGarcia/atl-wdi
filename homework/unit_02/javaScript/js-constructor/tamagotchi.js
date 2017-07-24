096b6a27c2d6a30acf68643631f2d45843565dd4
JaceGarcia <jaces.place@gmail.com>
2017-07-24 18:18:25 -0400

    part 3: added new methods

--- homework/unit_02/javaScript/js-constructor/tamagotchi.js
+++ homework/unit_02/javaScript/js-constructor/tamagotchi.js
@@ -5,15 +5,25 @@ class Tamagotchi{
   constructor(){
     this.name = name;
     this.creatureType = creatureType;
-    this.foodInTummy = 10;
-    this.restedness = 10;
-    this.health = 10;
+    this.foodInTummy = 100;
+    this.restedness = 100;
+    this.health = 100;
   }
 
   cry(){
     this.foodInTummy--;
     console.log("The Tamagotchi is crying!!! WAAAH!!!!!!");
     console.log('current food in tummy: ' + this.foodInTummy);
+  }
+  puke(){
+      this.health--;
+      console.log('Bleeeeh! Sick!!!!!!');
+      console.log(this.name + ' has current health = ' + this.health);
+  }
+  yawn(){
+      this.restedness--;
+      console.log('Yaaaawwwwn! So tired');
+      console.log(this.name + ' has current restedness = ' + this.restedness);
   }
 }
 
@@ -22,7 +32,7 @@ class Tamagotchi{
 var Neo = new Tamagotchi('Neo', 'theOne');
 var Morpheus = new Tamagotchi('Morpheus', 'theFinder');
 var Trinity = new Tamagotchi('Trinity', 'theSignificantOther');
-var Agent-Smith = new Tamagotchi('Agent-Smith', 'theAgent');
+var AgentSmith = new Tamagotchi('AgentSmith', 'theAgent');
 
 //test out your Tamagotchies below via console.logs
 console.log(Neo);
@@ -33,4 +43,4 @@ console.log(Agent-Smith);
 Neo.cry();
 Morpheus.cry();
 Trinity.cry();
-Agent-Smith();
+AgentSmith.cry();
