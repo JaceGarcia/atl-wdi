96d24d66477634784cedab2dc2934a3afaa49d18
JaceGarcia <jaces.place@gmail.com>
2017-07-25 18:37:11 -0400

    finished

---
+++ homework/unit_02/javaScript/js-data-modeling/homework.js
@@ -0,0 +1,40 @@
+// Rock Show
+var user1 = {
+    id: 1,
+    name: 'John Doe',
+    typeOfmusic: 'alternitive',
+    artist: [{
+        bandName: 'Linkin Park',
+        members: 'Chester', 'Mike', 'Joe', 'Rob', 'Brad', 'Dave',
+        topTracks: 'Numb', 'Bleed It Out', 'Some Where I Belong',
+    tours: [{
+        atlanta: 'Phillips Arena',
+        newYork: 'Madison Square',
+        tickets: [{
+            liveNation: 209,
+            ticketMaster: 210,
+        }]
+
+    }]
+}]
+}
+
+//Ultra E-Mail App
+
+var user2 = {
+    id: 2,
+    name: 'Jane Doe',
+    eMailProvider: [{
+        eMailCompany: 'google',
+        eName: 'janeDoe@gmail.com',
+    mailBox:[{
+            read: 3432,
+            unRead: 34322,
+            starred: 5,
+
+    }]
+    }]
+}
+
+//Final Thoughts
+'I just asked what people would click on and what information would be the most useful to the  client'
--- labs/unit_02/mongo/express-mongoose-lesson-starter/controllers/usersController.js
+++ labs/unit_02/mongo/express-mongoose-lesson-starter/controllers/usersController.js
@@ -5,15 +5,53 @@ var User = require("../models/user");
 var Item = require("../models/item");
 
 // USERS INDEX ROUTE
+router.get('/', (request, response) => {
+
+    User.find({})
+        .then((users) => {
+            response.render('users/index', {
+                users: users
+            });
+        })
+        .catch((error) => {
+            console.log(`ERROR!!!! ${error}`);
+        });
+
+})
 
 // USER SHOW ROUTE
-
+router.get('/:id', (req, res) =>{
+    user.findById(request.params.id)
+        .then((user) => {
+            response.render('users/show', {
+                user: user
+            })
+        })
+})
+// CREATE ROUTER FORM
+router.get('/new', (request, response) => {
+    response.render('users/new');
+})
 // USER CREATE ROUTE
-
+router.post('/', (request, response) => {
+    const newUser = new User(request.body);
+    newUser.save((err) => {
+        if (err) {
+            console.console.log('Error saving new user to data base' + err);
+        }
+    })
+})
 // USER UPDATE ROUTE
 
 // USER DESTROY
-
+router.get('/:id/delete', (request, response) => {
+    const userIdToDelete = request.params.id;
+    user.findByIdAndRemove(userIdToDelete)
+    .then(() =>{
+        console.log(`deleted user wit ID ${userIdToDelete}!`)
+        response.redirect('/users');
+    })
+});
 // ADD A NEW ITEM
 
 // REMOVE AN ITEM
